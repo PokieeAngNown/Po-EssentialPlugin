1ad8df1e8b0eccd3d0c84357154d50143f51479c
PokieeAngNown <100254514+PokieeAngNown@users.noreply.github.com>
2023-06-30 23:32:56 +0800

    Add files via upload

--- src/main/java/cn/PoStudio/EP.java
+++ src/main/java/cn/PoStudio/EP.java
@@ -1,24 +1,45 @@
 package cn.PoStudio;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public final class EP extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        Bukkit.getLogger().info("");
+
+        //插件硬前置检测
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") == null) {
+            getLogger().warning("|||Po-EP| Can't find the depend: PlaceholderAPI. Please download and install");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }else{
+            getLogger().warning("|||Po-EP| Found the depend: PlaceholderAPI" + Objects.requireNonNull(Bukkit.getPluginManager().getPlugin("PlaceholderAPI")).getDescription().getVersion());
+        }
+        getLogger().warning("|||Po-EP| Essential Plugin started!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().warning("|||Po-EP| Essential Plugin uninstalled!");
     }
 
     @Override
     public void onLoad(){
         // Plugin load logic
+        createLanguageJsonFile();
+        getLogger().warning("|||Po-EP| Essential Plugin on loading...");
+    }
 
+    Plugin plugin = this;
+
+    public void createLanguageJsonFile(){
+        this.saveResource("language/zh_CN.json", false);
     }
 }
